400a34e0c912ab29c1667c744b889cbb03e252aa
minhlee <minhlee.fre.mta@gmail.com>
2018-08-17 11:31:08 +0700

    add method login to UserController

--- src/com/codegym/controller/UserController.java
+++ src/com/codegym/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.codegym.controller;
 
 
+import com.codegym.dao.UserDao;
 import com.codegym.model.Login;
+import com.codegym.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 
@@ -15,4 +19,18 @@ public class UserController {
         ModelAndView modelAndView = new ModelAndView("home", "login", new Login());
         return modelAndView;
     }
+
+    @PostMapping("/login")
+    public ModelAndView login(@ModelAttribute("login") Login login) {
+        User user = UserDao.checkLogin(login);
+        if (user == null) {
+            ModelAndView modelAndView = new ModelAndView("Error!");
+            return modelAndView;
+        } else {
+            ModelAndView modelAndView = new ModelAndView("User");
+            modelAndView.addObject("user", user);
+            return modelAndView;
+        }
+    }
 }
+

fa4a666cddfa10c48125b145acfa42c07f0f91c7
minhlee <minhlee.fre.mta@gmail.com>
2018-08-17 12:04:41 +0700

    refactor code UserController

--- src/com/codegym/controller/UserController.java
+++ src/com/codegym/controller/UserController.java
@@ -24,10 +24,10 @@ public class UserController {
     public ModelAndView login(@ModelAttribute("login") Login login) {
         User user = UserDao.checkLogin(login);
         if (user == null) {
-            ModelAndView modelAndView = new ModelAndView("Error!");
+            ModelAndView modelAndView = new ModelAndView("error");
             return modelAndView;
         } else {
-            ModelAndView modelAndView = new ModelAndView("User");
+            ModelAndView modelAndView = new ModelAndView("user");
             modelAndView.addObject("user", user);
             return modelAndView;
         }
